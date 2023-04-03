4578dbc508f7c4abbc507620d07f69c17bbf7734
shrued <shrutisara10@gmail.com>
2023-04-03 01:50:54 -0400

    updates

--- gpt_song_suggestions.py
+++ gpt_song_suggestions.py
@@ -3,13 +3,11 @@ import openai
 
 openai.api_key = "OPENAI_API_KEY"
 
-genreDataObj = {}
-genreDataObj["whichTV"] = input("Enter a TV type (e.g. movie or tv show): ")
-genreDataObj["genre"] = input("Enter a genre (e.g. sci-fi, romance, etc.): ")
+musicGenre = input("Enter a genre to get a song recommendation (e.g. pop, rap, etc.): ")
 
 response = openai.Completion.create(
   model="text-davinci-003",
-  prompt=f"Suggest a {genreDataObj['whichTV']} in the {genreDataObj['genre']} genre:",
+  prompt=f"Suggest a song in the {musicGenre} genre:",
   temperature=0.9,
   max_tokens=150,
   top_p=1,
